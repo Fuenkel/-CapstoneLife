4ec9742dbd100f6ccbb5644949ae937aac4f2f01
ChoiSunHo <43162521+Fuenkel@users.noreply.github.com>
2019-11-26 18:15:23 +0900

    All Update

--- CapstoneLife/Assets/Scripts/DialogueFolder/Csh_Excel_Dialogue/InteractionEvent_Csh.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class InteractionEvent_Csh : MonoBehaviour
-{
-    [SerializeField] Dialogue_Csh dialogue_csh; 
-}
